0b49ada5e13ebdbd5ab8bfb2ae50287cafd5f215
Nick Baroni <nick@rhythnic.com>
2018-10-18 16:51:18 -0400

    Add missing dep to publish script.

--- bin/publish.js
+++ bin/publish.js
@@ -1,16 +1,12 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process')
 const { imageTags } = require('./lib/package')
+const path = require('path')
 
 const ROOT = path.join(__dirname, '..')
 const opts = { cwd: ROOT, stdio: 'inherit' }
 
-const {
-  DOCKER_USERNAME,
-  DOCKER_PASSWORD
-} = process.env
-
-execSync(`docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}`, opts)
+execSync(`echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin`, opts)
 execSync(`docker push ${imageTags.serverLatest}`, opts)
 execSync(`docker push ${imageTags.serverRc}`, opts)
 execSync(`docker push ${imageTags.e2eTest}`, opts)
